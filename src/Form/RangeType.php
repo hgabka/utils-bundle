4be56c53ff4b30b126a25bf7d5c012c51de53683
Gabe <hgabka@dev.hgabka.eu>
2018-02-19 19:37:30 +0100

    formok

---
+++ src/Form/RangeType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Hgabka\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * HTML5 range type field.
+ */
+class RangeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['attr' => ['min' => 0, 'max' => 100, 'step' => 1]]);
+    }
+
+    /**
+     * Get parent.
+     *
+     * @return string
+     */
+    public function getParent()
+    {
+        return IntegerType::class;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return 'range';
+    }
+}
---
+++ src/Form/WysiwygType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hgabka\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+/**
+ * Class WysiwygType.
+ */
+class WysiwygType extends AbstractType
+{
+    /**
+     * @return string
+     */
+    public function getParent()
+    {
+        return TextareaType::class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return 'wysiwyg';
+    }
+}
