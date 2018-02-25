57286aefb0cca057aff12095d689a4a5523715c8
Gabe <hgabka@dev.hgabka.eu>
2018-02-25 18:34:12 +0100

    simple media

--- src/Resources/public/js/hgabkautils.js
+++ src/Resources/public/js/hgabkautils.js
@@ -37,44 +37,6 @@ hgabkautils.ajaxModal = (function ($, window, undefined) {
 
 })(jQuery, window);
 
-hgabkautils.mediaChooser = (function(window, undefined) {
-
-    var init, initDelBtn;
-
-    var $body = $('body');
-
-    init = function() {
-        // Save and update preview can be found in url-chooser.js
-
-        initDelBtn();
-    };
-
-
-    // Del btn
-    initDelBtn = function() {
-        console.log($('.js-media-chooser-del-preview-btn').length);
-        $body.on('click', '.js-media-chooser-del-preview-btn', function(e) {
-            var $this = $(this),
-                linkedID = $this.data('linked-id'),
-                $widget = $('#' + linkedID + '-widget'),
-                $input = $('#' + linkedID);
-
-            $this.parent('.media-chooser__preview').find('.media-chooser__preview__img').attr({'src': '', 'srcset': '', 'alt': ''});
-
-            $(".media-thumbnail__icon").remove();
-
-            $widget.removeClass('media-chooser--choosen');
-            $input.val('');
-        });
-    };
-
-
-    return {
-        init: init
-    };
-
-})(window);
-
 
 hgabkautils.app = (function($, window, undefined) {
 
@@ -85,7 +47,6 @@ hgabkautils.app = (function($, window, undefined) {
     // General App init
     init = function() {
         hgabkautils.ajaxModal.init();
-        hgabkautils.mediaChooser.init();
     };
 
 
@@ -105,6 +66,7 @@ $(function() {
             linkedID = $this.data('linked-id'),
             $widget = $('#' + linkedID + '-widget'),
             $input = $('#' + linkedID);
+            $input2 = $('#' + linkedID + '_id');
 
         $this.parent('.media-chooser__preview').find('.media-chooser__preview__img').attr({'src': '', 'srcset': '', 'alt': ''});
 
@@ -112,6 +74,9 @@ $(function() {
 
         $widget.removeClass('media-chooser--choosen');
         $input.val('');
+        if ($input2.length) {
+            $input2.val('');
+        }
     });
 
 });
