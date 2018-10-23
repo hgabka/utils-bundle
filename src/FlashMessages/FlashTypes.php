7c935f61733bc6abb9244f60895a3a4d7e18a9f9
Gabe <hgabka@dev.hgabka.eu>
2018-10-23 12:28:49 +0200

    fix

---
+++ src/FlashMessages/FlashTypes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hgabka\UtilsBundle\FlashMessages;
+
+/**
+ * Class FlashTypes.
+ *
+ * Enabled flash types.
+ */
+class FlashTypes
+{
+    const SUCCESS = 'success';
+    const ERROR = 'error';
+    const WARNING = 'warning';
+
+    const INFO = 'info';
+    const DANGER = 'danger';
+}
