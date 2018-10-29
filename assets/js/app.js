a02409698671ce1c1f4d6be7ee72ab64ea42cf1f
Gabe <hgabka@dev.hgabka.eu>
2018-10-29 15:03:38 +0100

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 var hgabkautils = hgabkautils || {};
 var richeditor = require('./richeditor.js').richeditor;
+var slugChooser = require('./slugChooser.js').slugChooser;
+var urlChooser = require('./urlChooser.js').urlChooser;
 require('../css/ajax-modal.css');
+
 hgabkautils.app = (function($, window, undefined) {
 
     var init;
@@ -9,6 +12,8 @@ hgabkautils.app = (function($, window, undefined) {
     // General App init
     init = function() {
          richeditor.richEditor.init();
+         slugChooser.slugChooser.init();
+         urlChooser.urlChooser.init();
     };
 
 
