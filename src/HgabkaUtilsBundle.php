5815907bb61f79a519635f2745b47e9f929cc5c0
Gabe <hgabka@dev.hgabka.eu>
2018-11-03 10:13:36 +0100

    longblob

--- src/HgabkaUtilsBundle.php
+++ src/HgabkaUtilsBundle.php
@@ -2,8 +2,20 @@
 
 namespace Hgabka\UtilsBundle;
 
+use Doctrine\DBAL\Types\Type;
+use Hgabka\UtilsBundle\Doctrine\Type\LongblobType;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HgabkaUtilsBundle extends Bundle
 {
+    public function boot()
+    {
+        foreach ($this->container->getParameter('doctrine.entity_managers') as $name => $serviceName) {
+            $em = $this->container->get($serviceName);
+            if (!Type::hasType(LongblobType::TYPE)) {
+                Type::addType(LongblobType::TYPE, LongblobType::class);
+                $em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('longblob', LongblobType::TYPE);
+            }
+        }
+    }
 }
