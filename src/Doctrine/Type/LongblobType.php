576a01cef37600df662505b45af8e0f77a6cbc4d
Gabe <hgabka@dev.hgabka.eu>
2018-11-03 10:12:49 +0100

    longblob

---
+++ src/Doctrine/Type/LongblobType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Hgabka\UtilsBundle\Doctrine\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class LongblobType extends Type
+{
+    const TYPE = 'hg_utils_longblob';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return $platform->getBlobTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        return (string) $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        return (string) $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::TYPE;
+    }
+}
