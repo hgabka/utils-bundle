20fec6d5e837e76c881b4691ea82d1141849e238
Gabe <hgabka@dev.hgabka.eu>
2019-07-21 00:15:20 +0200

    dql substr

---
+++ src/DQL/Substr.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Hgabka\UtilsBundle\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class Substr extends FunctionNode
+{
+    const FUNCTION_NAME = 'SUBSTR';
+
+    /** @var \Doctrine\ORM\Query\AST\PathExpression */
+    protected $str;
+    /** @var \Doctrine\ORM\Query\AST\PathExpression */
+    protected $from;
+    /** @var \Doctrine\ORM\Query\AST\PathExpression */
+    protected $for;
+
+    /**
+     * @param SqlWalker $sqlWalker
+     *
+     * @return string
+     */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf(
+            'SUBSTR(%s FROM %s FOR %s)',
+            $this->str->dispatch($sqlWalker),
+            $this->from->dispatch($sqlWalker),
+            $this->for->dispatch($sqlWalker)
+        );
+    }
+
+    /**
+     * @param Parser $parser
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->str = $parser->StringPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->from = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->for = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+}
