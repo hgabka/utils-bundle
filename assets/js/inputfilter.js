6469a988f1c8235f8672205f98ece25640554e73
hgabka <hgabka@gmail.com>
2021-01-05 17:41:03 +0100

    Update inputfilter.js

--- assets/js/inputfilter.js
+++ assets/js/inputfilter.js
@@ -1,11 +1,16 @@
 (function($) {
     $.fn.inputFilter = function(inputFilter) {
         return this.on('input keydown keyup mousedown mouseup select contextmenu drop', function() {
+            let $this = $(this);
+            
             if (inputFilter(this.value)) {
                 this.oldValue = this.value;
                 this.oldSelectionStart = this.selectionStart;
                 this.oldSelectionEnd = this.selectionEnd;
-            } else if (this.hasOwnProperty("oldValue")) {
+            } else if (undefined !== $this.data('old-value')) {
+                this.value = $this.data('old-value');
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else if (this.hasOwnProperty('oldValue')) {
                 this.value = this.oldValue;
                 this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
             } else {
