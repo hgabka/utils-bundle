69008f9e58fff3a3a198b39cdc6b9a81d2815175
hgabka <hgabka@gmail.com>
2021-01-05 17:35:25 +0100

    Create inputfilter.js

---
+++ assets/js/inputfilter.js
@@ -0,0 +1,16 @@
+(function($) {
+    $.fn.inputFilter = function(inputFilter) {
+        return this.on('input keydown keyup mousedown mouseup select contextmenu drop', function() {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    };
+}(jQuery));
