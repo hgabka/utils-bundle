632a75fc74e2ca388044dbc9878c875073619cd4
hgabka <hgabka@gmail.com>
2021-03-29 08:45:55 +0200

    Create TaxIdValidator.php

---
+++ src/Validator/Constraints/TaxIdValidator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Hgabka\UtilsBundle\Validator\Constraint;
+
+use Hgabka\UtilsBundle\Validator\TaxValidator;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class TaxIdValidator extends ConstraintValidator
+{
+    /** @var TaxValidator */
+    protected $taxValidator;
+
+    /**
+     * TaxNoValidator constructor.
+     *
+     * @param TaxValidator $taxValidator
+     */
+    public function __construct(TaxValidator $taxValidator)
+    {
+        $this->taxValidator = $taxValidator;
+    }
+
+    public function validate($value, Constraint $constraint)
+    {
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!$this->taxValidator->validateTaxId($value)) {
+            $this->context->buildViolation($constraint->message)
+                          ->addViolation();
+        }
+    }
+}
