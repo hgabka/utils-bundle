aa0b6c95cae013296ca65f9fd02394f9e2050570
hgabka <hgabka@gmail.com>
2021-09-29 15:38:36 +0200

    Update jquery.inputfilter.js

--- assets/js/jquery.inputfilter.js
+++ assets/js/jquery.inputfilter.js
@@ -6,7 +6,7 @@
                 this.oldValue = this.value;
                 this.oldSelectionStart = this.selectionStart;
                 this.oldSelectionEnd = this.selectionEnd;
-            } else if (this.hasOwnProperty("oldValue")) {
+            } else if (this.hasOwnProperty('oldValue')) {
                 this.value = this.oldValue;
                 if ('number' !== this.type) {
                     this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
