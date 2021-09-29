b2c51df7c4dba16206bb4e9a9ed77c64098d97bb
hgabka <hgabka@gmail.com>
2021-09-29 15:37:58 +0200

    Update jquery.inputfilter.js

--- assets/js/jquery.inputfilter.js
+++ assets/js/jquery.inputfilter.js
@@ -1,16 +1,19 @@
 (function($) {
     $.fn.inputFilter = function(inputFilter) {
         return this.on('input keydown keyup mousedown mouseup select contextmenu drop', function() {
-            if (inputFilter(this.value)) {
+            let valid = !(this.validity && !this.validity.valid);
+            if (valid && inputFilter(this.value)) {
                 this.oldValue = this.value;
                 this.oldSelectionStart = this.selectionStart;
                 this.oldSelectionEnd = this.selectionEnd;
             } else if (this.hasOwnProperty("oldValue")) {
                 this.value = this.oldValue;
-                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+                if ('number' !== this.type) {
+                    this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+                }
             } else {
                 this.value = '';
             }
         });
     };
-}(jQuery));
+}($));
