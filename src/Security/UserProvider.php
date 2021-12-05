b52e0d0797c10eea060669fc05e114d6d8d0a844
hgabka <hgabka@gmail.com>
2021-12-05 13:06:05 +0100

    Create UserProvider.php

---
+++ src/Security/UserProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Hgabka\UtilsBundle\Security;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Hgabka\UtilsBundle\Entity\UserInterface as BundleUserInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+final class UserProvider implements UserProviderInterface
+{
+    protected $userClass;
+
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, string $userClass)
+    {
+        $this->entityManager = $entityManager;
+        $this->userClass = $userClass;
+    }
+
+    public function loadUserByUsername(string $username): User
+    {
+        $user = $this->findOneUserBy(['email' => $username]);
+
+        if (!$user) {
+            throw new AuthenticationException(
+                sprintf(
+                    'User with "%s" email does not exist.',
+                    $username
+                )
+            );
+        }
+
+        return $user;
+    }
+
+    private function findOneUserBy(array $options): ?User
+    {
+        return
+            $this
+                ->entityManager
+                ->getRepository($this->userClass)
+                ->findOneBy($options)
+        ;
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        assert($user instanceof BundleUserInterface);
+
+        if (null === $reloadedUser = $this->findOneUserBy(['id' => $user->getId()])) {
+            throw new AuthenticationException(sprintf(
+                'User with ID "%s" could not be reloaded.',
+                $user->getId()
+            ));
+        }
+
+        return $reloadedUser;
+    }
+
+    public function supportsClass(string $class): bool
+    {
+        return $class === User::class;
+    }
+}
