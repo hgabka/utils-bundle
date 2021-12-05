632970382bb6f785c79ae2529ffa92f9fcfc9d2b
hgabka <hgabka@gmail.com>
2021-12-05 12:57:57 +0100

    Create AdminLoginAuthenticator.php

---
+++ src/Security/AdminLoginAuthenticator.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Hgabka\UtilsBundle\Security;
+
+use Hgabka\UtilsBundle\Form\AdminLoginForm;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Guard\AuthenticatorInterface;
+
+final class AdminLoginAuthenticator extends AbstractFormLoginAuthenticator implements AuthenticatorInterface
+{
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(
+        FormFactoryInterface $formFactory,
+        RouterInterface $router,
+        UserPasswordHasherInterface $passwordEncoder
+    ) {
+        $this->formFactory = $formFactory;
+        $this->router = $router;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function supports(Request $request): bool
+    {
+        return $request->attributes->get('_route') === 'admin_login' && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request): array
+    {
+        $form = $this->formFactory->create(AdminLoginForm::class);
+        $form->handleRequest($request);
+
+        $data = $form->getData();
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $data['email']
+        );
+
+        return $data;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider): UserInterface
+    {
+        return $userProvider->loadUserByUsername($credentials['email']);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): RedirectResponse
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        return new RedirectResponse($this->router->generate('admin_login'));
+    }
+
+    protected function getLoginUrl(): string
+    {
+        return $this->router->generate('admin_login');
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey): RedirectResponse
+    {
+        return new RedirectResponse($this->router->generate('sonata_admin_dashboard'));
+    }
+}
