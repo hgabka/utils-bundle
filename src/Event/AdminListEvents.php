c9be60ad562f0f1ceb423561b3e259360ce973f5
hgabka <hgabka@gmail.com>
2021-12-11 07:15:51 +0100

    Update AdminListEvents.php

--- src/Event/AdminListEvents.php
+++ src/Event/AdminListEvents.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kunstmaan\AdminListBundle\Event;
+namespace Hgabka\UtilsBundleBundle\Event;
 
 class AdminListEvents
 {
@@ -8,31 +8,31 @@ class AdminListEvents
      * When adding, triggered after the form is validated,
      * but before the entity is persisted.
      */
-    public const PRE_ADD = 'kunstmaan_admin_list.preAdd';
+    public const PRE_ADD = 'hgabka_utils.preAdd';
 
     /**
      * When adding, triggered after the entity is flushed.
      */
-    public const POST_ADD = 'kunstmaan_admin_list.postAdd';
+    public const POST_ADD = 'hgabka_utils.postAdd';
 
     /**
      * When editing, triggered after the form is validated,
      * but before the entity is persisted.
      */
-    public const PRE_EDIT = 'kunstmaan_admin_list.preEdit';
+    public const PRE_EDIT = 'hgabka_utils.preEdit';
 
     /**
      * When editing, triggered after the entity is flushed.
      */
-    public const POST_EDIT = 'kunstmaan_admin_list.postEdit';
+    public const POST_EDIT = 'hgabka_utils.postEdit';
 
     /**
      * When deleting, triggered before the entity is removed.
      */
-    public const PRE_DELETE = 'kunstmaan_admin_list.preDelete';
+    public const PRE_DELETE = 'hgabka_utils.preDelete';
 
     /**
      * When deleting, triggered after the remove is flushed.
      */
-    public const POST_DELETE = 'kunstmaan_admin_list.postDelete';
+    public const POST_DELETE = 'hgabka_utils.postDelete';
 }
