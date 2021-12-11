62adc6e448b754a8383de0c188d2917eb58d78fe
hgabka <hgabka@gmail.com>
2021-12-11 07:14:39 +0100

    Update AdminListEvent.php

--- src/Event/AdminListEvent.php
+++ src/Event/AdminListEvent.php
@@ -2,7 +2,7 @@
 
 namespace Kunstmaan\AdminListBundle\Event;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
