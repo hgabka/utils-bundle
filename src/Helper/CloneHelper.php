9524e66f5e20e2f19f00e2a92a37540485e6890a
hgabka <hgabka@gmail.com>
2021-12-11 07:13:22 +0100

    Update CloneHelper.php

--- src/Helper/CloneHelper.php
+++ src/Helper/CloneHelper.php
@@ -37,12 +37,12 @@ class CloneHelper
     public function deepCloneAndSave($entity)
     {
         $clonedEntity = clone $entity;
-        $this->eventDispatcher->dispatch(Events::DEEP_CLONE_AND_SAVE, new DeepCloneAndSaveEvent($entity, $clonedEntity, $this->em));
+        $this->eventDispatcher->dispatch(new DeepCloneAndSaveEvent($entity, $clonedEntity, $this->em), Events::DEEP_CLONE_AND_SAVE);
 
         $this->em->persist($clonedEntity);
         $this->em->flush();
 
-        $this->eventDispatcher->dispatch(Events::POST_DEEP_CLONE_AND_SAVE, new DeepCloneAndSaveEvent($entity, $clonedEntity, $this->em));
+        $this->eventDispatcher->dispatch(new DeepCloneAndSaveEvent($entity, $clonedEntity, $this->em), Events::POST_DEEP_CLONE_AND_SAVE);
 
         return $clonedEntity;
     }
