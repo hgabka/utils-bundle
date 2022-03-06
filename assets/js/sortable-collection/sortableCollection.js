9dc4517c7debde88dc18caee65fc02ae909a3d40
hgabka <hgabka@gmail.com>
2022-03-06 10:34:49 +0100

    Update sortableCollection.js

--- assets/js/sortable-collection/sortableCollection.js
+++ assets/js/sortable-collection/sortableCollection.js
@@ -2,30 +2,7 @@
 
 require('jquery-ui-sortable');
 let ajaxModal = require('../ajaxmodal.js').ajaxModal;
-
-$.fn.swapWith = function (that, animate) {
-    var $this = this;
-    var $that = $(that);
-
-    // create temporary placeholder
-    var $temp = $("<div>");
-
-    if ('undefined' !== typeof animate && animate) {
-        $this.hide();
-        $that.hide();
-        $this.before($temp);
-        $that.before($this);
-        $this.fadeIn({duration: 'slow', easing: 'linear'});
-        $that.fadeIn({duration: 'slow', easing: 'linear'});
-    } else {
-        // 3-step swap
-        $this.before($temp);
-        $that.before($this);
-    }
-    $temp.after($that).remove();
-
-    return $this;
-}
+require('../swapWith.js');
 
 class SortableCollectionHandler
 {
