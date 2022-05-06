12fcb49dc8daff4ece9a640938696f2d5b7307c6
hgabka <hgabka@gmail.com>
2022-05-06 13:49:13 +0200

    Update Round.php

--- src/DQL/Round.php
+++ src/DQL/Round.php
@@ -17,7 +17,7 @@ class Round extends FunctionNode
     /**
      * {@inheritdoc}
      */
-    public function parse(Parser $parser)
+    public function parse(Parser $parser): void
     {
         $lexer = $parser->getLexer();
         $parser->match(Lexer::T_IDENTIFIER);
@@ -31,7 +31,7 @@ class Round extends FunctionNode
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 
-    public function getSql(SqlWalker $sqlWalker)
+    public function getSql(SqlWalker $sqlWalker): string
     {
         return 'ROUND(' . $this->arithmeticExpression->dispatch($sqlWalker) . (null !== $this->decimals ? ', ' . $this->decimals->dispatch($sqlWalker) : '') . ')';
     }
