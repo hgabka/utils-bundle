015c066582040b8f236faac15c49c3e0d0c67d81
hgabka <hgabka@gmail.com>
2022-05-06 13:47:17 +0200

    Update IfElse.php

--- src/DQL/IfElse.php
+++ src/DQL/IfElse.php
@@ -13,7 +13,7 @@ class IfElse extends FunctionNode
 
     private $expr = [];
 
-    public function parse(Parser $parser)
+    public function parse(Parser $parser): void
     {
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
@@ -27,7 +27,7 @@ class IfElse extends FunctionNode
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 
-    public function getSql(SqlWalker $sqlWalker)
+    public function getSql(SqlWalker $sqlWalker): string
     {
         return sprintf(
             'IF(%s, %s, %s)',
