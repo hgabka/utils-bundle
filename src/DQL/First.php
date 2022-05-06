5952acb6dedf9f0dc39170039fc094394e5e3657
hgabka <hgabka@gmail.com>
2022-05-06 13:46:39 +0200

    Update First.php

--- src/DQL/First.php
+++ src/DQL/First.php
@@ -24,7 +24,7 @@ class First extends FunctionNode
     /**
      * {@inheritdoc}
      */
-    public function parse(Parser $parser)
+    public function parse(Parser $parser): void
     {
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
@@ -35,7 +35,7 @@ class First extends FunctionNode
     /**
      * {@inheritdoc}
      */
-    public function getSql(SqlWalker $sqlWalker)
+    public function getSql(SqlWalker $sqlWalker): string
     {
         return '(' . $this->subselect->dispatch($sqlWalker) . ' LIMIT 1)';
     }
