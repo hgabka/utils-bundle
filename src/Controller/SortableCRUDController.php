f5bff84faaf5b2e9b572793748541f1391de2816
hgabka <hgabka@gmail.com>
2022-05-13 13:16:30 +0200

    Update SortableCRUDController.php

--- src/Controller/SortableCRUDController.php
+++ src/Controller/SortableCRUDController.php
@@ -2,6 +2,7 @@
 
 namespace Hgabka\UtilsBundle\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,7 +10,7 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class SortableCRUDController extends CRUDController
 {
-    public function sortingAction(Request $request): Response
+    public function sortingAction(Request $request, ManagerRegistry $doctrine): Response
     {
         if (!$request->isXmlHttpRequest()) {
             throw $this->createNotFoundException();
@@ -24,14 +25,14 @@ class SortableCRUDController extends CRUDController
         }
         $propertyAccessor = PropertyAccess::createPropertyAccessor();
 
-        foreach ($request->request->get('positions') as $id => $position) {
+        foreach ($request->request->all('positions') as $id => $position) {
             $object = $this->admin->getObject($id);
             if ($object) {
                 $propertyAccessor->setValue($object, $this->admin->getSortField(), $position);
             }
         }
 
-        $this->getDoctrine()->getManager()->flush();
+        $doctrine->getManager()->flush();
 
         return new Response();
     }
