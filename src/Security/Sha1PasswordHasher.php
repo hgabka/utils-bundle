8fca5da59ee4ae3f0d2bcaeabb8a1266801c8af0
hgabka <hgabka@gmail.com>
2022-05-20 08:39:15 +0200

    Create Sha1PasswordHasher.php

---
+++ src/Security/Sha1PasswordHasher.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\PasswordHasher\LegacyPasswordHasherInterface;
+
+class Sha1PasswordHasher implements LegacyPasswordHasherInterface
+{
+    public function hash(string $plainPassword, string $salt = null): string
+    {
+        return sha1($salt.$plainPassword);
+    }
+
+    public function verify(string $hashedPassword, string $plainPassword, string $salt = null): bool
+    {
+        return $hashedPassword === sha1($salt.$plainPassword);
+    }
+
+    public function needsRehash(string $hashedPassword): bool
+    {
+        return true;
+    }
+
+}
