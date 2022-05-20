a0549b91cd0eff34028de1249947f5c8ed297f81
hgabka <hgabka@gmail.com>
2022-05-20 14:43:38 +0200

    Update Sha1PasswordHasher.php

--- src/Security/Sha1PasswordHasher.php
+++ src/Security/Sha1PasswordHasher.php
@@ -2,17 +2,27 @@
 
 namespace Hgabka\UtilsBundle\Security;
 
+use Symfony\Component\PasswordHasher\Exception\InvalidPasswordException;
+use Symfony\Component\PasswordHasher\Hasher\CheckPasswordLengthTrait;
 use Symfony\Component\PasswordHasher\LegacyPasswordHasherInterface;
 
 class Sha1PasswordHasher implements LegacyPasswordHasherInterface
 {
     public function hash(string $plainPassword, string $salt = null): string
     {
+        if ($this->isPasswordTooLong($plainPassword)) {
+            throw new InvalidPasswordException();
+        }
+        
         return sha1($salt.$plainPassword);
     }
 
     public function verify(string $hashedPassword, string $plainPassword, string $salt = null): bool
     {
+        if ('' === $plainPassword || $this->isPasswordTooLong($plainPassword)) {
+            return false;
+        }
+        
         return $hashedPassword === sha1($salt.$plainPassword);
     }
 
