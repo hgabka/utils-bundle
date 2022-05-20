7b9dee494fe254999deaf29c8453a7694d498724
hgabka <hgabka@gmail.com>
2022-05-20 14:42:46 +0200

    Update Md5PasswordHasher.php

--- src/Security/Md5PasswordHasher.php
+++ src/Security/Md5PasswordHasher.php
@@ -2,17 +2,27 @@
 
 namespace Hgabka\UtilsBundle\Security;
 
+use Symfony\Component\PasswordHasher\Exception\InvalidPasswordException;
+use Symfony\Component\PasswordHasher\Hasher\CheckPasswordLengthTrait;
 use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 
 class Md5PasswordHasher implements PasswordHasherInterface
 {
     public function hash(string $plainPassword): string
     {
+        if ($this->isPasswordTooLong($plainPassword)) {
+            throw new InvalidPasswordException();
+        }
+
         return md5($plainPassword);
     }
 
     public function verify(string $hashedPassword, string $plainPassword): bool
     {
+        if ('' === $plainPassword || $this->isPasswordTooLong($plainPassword)) {
+            return false;
+        }
+        
         return $hashedPassword === md5($plainPassword);
     }
 
