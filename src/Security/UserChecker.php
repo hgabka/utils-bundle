1bb4a8a99e20ceae3a228b8845796c1bd155da2c
hgabka <hgabka@gmail.com>
2022-05-26 06:22:35 +0200

    Create UserChecker.php

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Hgabka\UtilsBundle\Security;
+
+use Hgabka\UtilsBundle\Model\AbstractUser;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function __construct(private readonly TranslatorInterface $translator) {}
+
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof AbstractUser) {
+            return;
+        }
+
+        if (!$user->isEnabled()) {
+            // the message passed to this exception is meant to be displayed to the user
+            throw new CustomUserMessageAccountStatusException($this->translator->trans('hg_utils.security.error_not_enabled.'));
+        }
+    }
+}
