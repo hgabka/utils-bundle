2c80ca6519054903a99e865cbb831970e33ad20b
hgabka <hgabka@gmail.com>
2022-11-06 18:29:21 +0100

    Update Recaptcha.php

--- src/Validator/Constraints/Recaptcha.php
+++ src/Validator/Constraints/Recaptcha.php
@@ -4,9 +4,7 @@ namespace Hgabka\UtilsBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 
-/**
- * @Annotation
- */
+#[\Attribute]
 class Recaptcha extends Constraint
 {
     public string $message = 'hg_utils.recaptcha.message';
@@ -15,12 +13,15 @@ class Recaptcha extends Constraint
 
     public float $minimumScore = 0.2;
 
+    public string $action = 'utils';
+
     #[HasNamedArguments]
-    public function __construct(string $mode = 'normal', float $minimumScore = 0.2, array $groups = null, mixed $payload = null)
+    public function __construct(string $mode = 'normal', float $minimumScore = 0.2, ?string $action = 'utils', array $groups = null, mixed $payload = null)
     {
         parent::__construct([], $groups, $payload);
 
         $this->mode = $mode;
         $this->minimumScore = $minimumScore;
+        $this->action = $action;
     }
 }
