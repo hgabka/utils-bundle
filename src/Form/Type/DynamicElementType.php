a07653f99b281643812811ee00829d81d159b2df
hgabka <hgabka@gmail.com>
2022-12-04 11:18:33 +0100

    Update DynamicElementType.php

--- src/Form/Type/DynamicElementType.php
+++ src/Form/Type/DynamicElementType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class DynamicElementType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('id', HiddenType::class)
@@ -16,7 +16,7 @@ class DynamicElementType extends AbstractType
         ;
     }
 
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return 'dynamic_element';
     }
