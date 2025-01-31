21042d79373e38d02785261532f42a9c7716ad57
hgabka <hgabka@gmail.com>
2025-01-31 19:53:55 +0100

    Update HgabkaUtilsExtension.php

--- src/DependencyInjection/HgabkaUtilsExtension.php
+++ src/DependencyInjection/HgabkaUtilsExtension.php
@@ -28,10 +28,10 @@ use Hgabka\UtilsBundle\Helper\Menu\MenuBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
